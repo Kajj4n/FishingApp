add9f18aa40c6dc8c65558fd6af098c7341043b5
Kajj4n <143015848+Kajj4n@users.noreply.github.com>
2026-03-17 19:52:37 +0100

    yup

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // CHANGE THIS VERSION NUMBER every time you update your CSS, HTML, or JS!
-const CACHE_NAME = "guitar-v13"; 
+const CACHE_NAME = "guitar-v14"; 
 const ASSETS = ["/", "/index.html", "/style.css", "/app.js", "/manifest.json"];
 
 self.addEventListener("install", (e) => {
